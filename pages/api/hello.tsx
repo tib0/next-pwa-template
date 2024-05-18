88d6ad7459e4805b5ecbaee78791d785e48a9490
Tib0 <4764729+tib0@users.noreply.github.com>
2024-05-18 23:45:38 +0200

    remove comment

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
 
 const hello = (_req: NextApiRequest, res: NextApiResponse) => {
